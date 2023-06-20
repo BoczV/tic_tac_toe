70d10e8e38f9db90933796f01af2b799cecc348b
Viktor <beviktor95@gmail.com>
2023-06-20 13:09:47 +0200

    More refactors: optimize for cycles I.

--- playing_logic/game.py
+++ playing_logic/game.py
@@ -15,65 +15,66 @@ class Game:
     player = None
     robot = None
     menu = None
+    size = 0
+    difficulty_level = None
 
     def __init__(self):
-        self.win_inspector = Win_inspector()
         self.player = Player("X")
         self.menu = Menu()
 
 
     def start_game(self):
         board_size_char = self.menu.get_game_board_size_answer()
-        board_size = self.board_size_swithcer[board_size_char]
-        self.init_board_record(board_size)
+        self.size = self.board_size_swithcer[board_size_char]
+        self.init_board_record()
         self.robot.setup_informations_for_robot()
-        return board_size
 
 
-    def play(self, size, difficulty_level):
+    def play(self):
         player_value = "X"
         someone_won = False
         who_won = None
         while "." in self.board_record.values() and not someone_won:
-            self.play_one_round(player_value, difficulty_level)
+            self.play_one_round(player_value)
             player_value = "O" if player_value == "X" else "X"
-            who_won = self.win_inspector.check_if_someone_wins(size, self.needed_part_of_alphabet, self.board_record)
+            who_won = self.win_inspector.check_if_someone_wins(self.board_record)
             someone_won = who_won is not None
         self.menu.finish_game(who_won, self.needed_part_of_alphabet, self.board_record)
 
 
-    def play_one_round(self, player_value, difficulty_level):
+    def play_one_round(self, player_value):
         if player_value == "X":
             self.player.player_move(self.needed_part_of_alphabet, self.board_record)
         else:
-            self.robot_round(difficulty_level)
+            self.robot_round()
 
 
-    def robot_round(self, difficulty_level):
-        if difficulty_level == "a":
+    def robot_round(self):
+        if self.difficulty_level == "a":
                 self.robot.robot_move_easy_level(self.board_record)
-        elif difficulty_level == "b":
+        elif self.difficulty_level == "b":
             self.robot.robot_move_medium_level(self.board_record)
         else:
             self.robot.robot_move_impossible_level(self.board_record)
 
 
-    def init_board_record(self, size):
-        for i in range(size):
+    def init_board_record(self):
+        for i in range(self.size):
             letter = self.alphabet[i]
-            for j in range(1, size + 1):
+            for j in range(1, self.size + 1):
                 key = f"{letter}{j}"
                 self.board_record[key] = "."
             self.needed_part_of_alphabet.append(letter)
-        self.robot = Robot("O", self.needed_part_of_alphabet, size)
+        self.robot = Robot("O", self.needed_part_of_alphabet, self.size)
+        self.win_inspector = Win_inspector(self.needed_part_of_alphabet, self.size)
 
 
     def game(self):
         start_answer = self.menu.get_start_answer()
         os.system('clear')
         if start_answer == "y":
-            board_size = self.start_game()
-            difficulty_level = self.menu.get_difficulty_level()
-            self.play(board_size, difficulty_level)
+            self.start_game()
+            self.difficulty_level = self.menu.get_difficulty_level()
+            self.play()
         else:
             print("Okay, bye!")
--- playing_logic/win_inspector.py
+++ playing_logic/win_inspector.py
@@ -1,74 +1,72 @@
 
 class Win_inspector:
-    value_switcher = {"user": "X", "robot": "O"}
+    value_switcher = None
+    needed_part_of_alphabet = None
+    size = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, needed_part_of_alphabet, size):
+        self.needed_part_of_alphabet = needed_part_of_alphabet
+        self.size = size
+        self.value_switcher = {"user": "X", "robot": "O"}
 
 
-    def check_if_actor_wins(self, player_value, size, needed_part_of_alphabet, board_record):
-        if self.check_if_someone_wins_horizontally(player_value, size, needed_part_of_alphabet, board_record):
+    def check_if_actor_wins(self, player_value, board_record):
+        if self.check_if_someone_wins_horizontally(player_value, board_record):
             return True
-        elif self.check_if_someone_wins_vertically(player_value, size, needed_part_of_alphabet, board_record):
+        elif self.check_if_someone_wins_vertically(player_value, board_record):
             return True
-        elif self.check_if_someone_wins_cross_diagonally(player_value, size, needed_part_of_alphabet, board_record):
+        elif self.check_if_someone_wins_cross_diagonally(player_value, board_record):
             return True
         return False
 
 
-    def check_if_someone_wins(self, size, needed_part_of_alphabet, board_record):
+    def check_if_someone_wins(self, board_record):
         result = None
-        if self.check_if_actor_wins(self.value_switcher["user"], size, needed_part_of_alphabet, board_record):
+        if self.check_if_actor_wins(self.value_switcher["user"], board_record):
             result = self.value_switcher["user"]
-        elif self.check_if_actor_wins(self.value_switcher["robot"], size, needed_part_of_alphabet, board_record):
+        elif self.check_if_actor_wins(self.value_switcher["robot"], board_record):
             result = self.value_switcher["robot"]
         return result
 
 
-    def check_if_someone_wins_vertically(self, player_value, size, needed_part_of_alphabet, board_record):
+    def check_if_someone_wins_vertically(self, player_value, board_record):
         counter = 0
-        for i in needed_part_of_alphabet:
-            for j in range(1, len(needed_part_of_alphabet) + 1):
+        for i in self.needed_part_of_alphabet:
+            for j in range(1, self.size + 1):
                 key = f"{i}{j}"
                 if board_record[key] == player_value:
                     counter += 1
-            if counter == size:
+            if counter == self.size:
                 return True
             else:
                 counter = 0
         return False
 
 
-    def check_if_someone_wins_horizontally(self, player_value, size, needed_part_of_alphabet, board_record):
+    def check_if_someone_wins_horizontally(self, player_value, board_record):
         counter = 0
-        for j in range(1, len(needed_part_of_alphabet) + 1):
-            for i in needed_part_of_alphabet:
+        for j in range(1, self.size + 1):
+            for i in self.needed_part_of_alphabet:
                 key = f"{i}{j}"
                 if board_record[key] == player_value:
                     counter += 1
-            if counter == size:
+            if counter == self.size:
                 return True
             else:
                 counter = 0
         return False
 
 
-    def check_if_someone_wins_cross_diagonally(self, player_value, size, needed_part_of_alphabet, board_record):
-        counter = 0
-        for i in range(len(needed_part_of_alphabet)):
-            key = f"{needed_part_of_alphabet[i] + str(i + 1)}"
-            if board_record[key] == player_value:
-                counter += 1
-        if counter == size:
+    def check_if_someone_wins_cross_diagonally(self, player_value, board_record):
+        counter1 = 0
+        counter2 = 0
+        for i in range(self.size):
+            key1 = f"{self.needed_part_of_alphabet[i] + str(i + 1)}"
+            key2 = f"{self.needed_part_of_alphabet[self.size - 1 - i] + str(i + 1)}"
+            if board_record[key1] == player_value:
+                counter1 += 1
+            if board_record[key2] == player_value:
+                counter2 += 1
+        if counter1 == self.size or counter2 == self.size:
                 return True
-        else:
-            counter = 0
-
-        for j in range(len(needed_part_of_alphabet)):
-            key = f"{needed_part_of_alphabet[len(needed_part_of_alphabet) - 1 - j] + str(j + 1)}"
-            if board_record[key] == player_value:
-                counter += 1
-        if counter == size:
-                return True
-        else:
-            counter = 0
+        return False
