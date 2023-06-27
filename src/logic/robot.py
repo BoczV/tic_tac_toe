9fb6382864c3d6e7bfe3e17a3951ae269df1f3d0
Viktor <beviktor95@gmail.com>
2023-06-27 23:36:56 +0200

    Fixed instant winning logic

--- src/logic/robot.py
+++ src/logic/robot.py
@@ -174,10 +174,13 @@ class Robot:
     def find_an_instant_winner_path(self, board_record: dict) -> list:
         for possible_winning_option in self.possible_winning_options:
             counter = 0
+            opponent_counter = 0
             for i in possible_winning_option:
                 element = board_record[i]
                 if element == self.player_value:
                     counter += 1
-            if counter == self.size - 1:
+                if element == self.opponent_value:
+                    opponent_counter += 1
+            if counter == self.size - 1 and opponent_counter == 0:
                 return possible_winning_option
         return []
