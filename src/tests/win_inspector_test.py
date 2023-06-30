b1d8d70b0fb62e4be4e1e08bd0e0683fbaa2c507
Bocz Viktor <viktor.bocz@smp.hu>
2023-06-30 17:21:16 +0200

    Created default tests

--- src/tests/win_inspector_test.py
+++ src/tests/win_inspector_test.py
@@ -15,20 +15,57 @@ class Test_Win_inspector:
 
 
     @pytest.fixture
-    def get_winning_board_record(self) -> dict:
-        return {"a1": "X", "a2": ".", "a3": ".", "b1": "O", "b2": "X", "b3": "O", "c1": "O", "c2": ".", "c3": "X"}
+    def get_diagonally_board_record(self) -> dict:
+        return {
+            "a1": "X", "a2": ".", "a3": ".", 
+            "b1": "O", "b2": "X", "b3": "O", 
+            "c1": "O", "c2": ".", "c3": "X"
+            }
 
 
     @pytest.fixture
-    def get_in_progress_board_record(self) -> dict:
-        return {"a1": ".", "a2": "X", "a3": ".", "b1": "O", "b2": "X", "b3": "O", "c1": "O", "c2": ".", "c3": "X"}
+    def get_vertically_board_record(self) -> dict:
+        return {
+            "a1": ".", "a2": "X", "a3": "O", 
+            "b1": "O", "b2": "X", "b3": "O", 
+            "c1": "O", "c2": "X", "c3": "X"
+            }
+    
+
+    @pytest.fixture
+    def get_horizontally_board_record(self) -> dict:
+        return {
+            "a1": "O", "a2": "O", "a3": "O", 
+            "b1": "X", "b2": ".", "b3": ".", 
+            "c1": "X", "c2": "X", "c3": "."
+            }
 
 
-    def test_check_if_someone_wins_vertically_returns_true_for_user(self, get_winning_board_record: dict):
-        win = self.win_inspector.check_if_someone_wins_vertically("X", get_winning_board_record)
+    def test_check_if_someone_wins_diagonally_returns_true_for_user(self, get_diagonally_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_cross_diagonally("X", get_diagonally_board_record)
         assert win is True
 
 
-    def test_check_if_someone_wins_vertically_returns_false(self, get_in_progress_board_record: dict):
-        win = self.win_inspector.check_if_someone_wins_vertically("X", get_in_progress_board_record)
+    def test_check_if_someone_wins_diagonally_returns_false_for_robot(self, get_diagonally_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_cross_diagonally("O", get_diagonally_board_record)
         assert win is False
+
+
+    def test_check_if_someone_wins_vertically_returns_true_for_user(self, get_vertically_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_vertically("X", get_vertically_board_record)
+        assert win is True
+
+    
+    def test_check_if_someone_wins_vertically_returns_false_for_robot(self, get_vertically_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_vertically("O", get_vertically_board_record)
+        assert win is False
+
+    
+    def test_check_if_someone_wins_horizontally_returns_false_for_user(self, get_horizontally_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_horizontally("X", get_horizontally_board_record)
+        assert win is False
+
+
+    def test_check_if_someone_wins_horizontally_returns_true_for_robot(self, get_horizontally_board_record: dict):
+        win = self.win_inspector.check_if_someone_wins_horizontally("O", get_horizontally_board_record)
+        assert win is True
