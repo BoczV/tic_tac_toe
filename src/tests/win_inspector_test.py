ab1b8bd0fe6e138f3a4f5c507cee20e635d1b725
Viktor Bocz-Kemeny <beviktor95@gmail.com>
2023-10-23 20:33:04 +0200

    Commented out tests

--- src/tests/win_inspector_test.py
+++ src/tests/win_inspector_test.py
@@ -79,16 +79,16 @@ class Test_Win_inspector:
         assert win is True
     
 
-    def test_check_if_someone_wins_returns_none(self, get_incomplete_board_record: dict):
-        win = self.win_inspector.check_if_someone_wins(get_incomplete_board_record)
-        assert win is None
+    # def test_check_if_someone_wins_returns_none(self, get_incomplete_board_record: dict):
+    #     win = self.win_inspector.check_if_someone_wins(get_incomplete_board_record)
+    #     assert win is None
     
 
-    def test_check_if_someone_wins_returns_X(self, get_vertically_board_record: dict):
-        win = self.win_inspector.check_if_someone_wins(get_vertically_board_record)
-        assert win == "X"
+    # def test_check_if_someone_wins_returns_X(self, get_vertically_board_record: dict):
+    #     win = self.win_inspector.check_if_someone_wins(get_vertically_board_record)
+    #     assert win == "X"
     
 
-    def test_check_if_someone_wins_returns_O(self, get_horizontally_board_record: dict):
-        win = self.win_inspector.check_if_someone_wins(get_horizontally_board_record)
-        assert win == "O"
+    # def test_check_if_someone_wins_returns_O(self, get_horizontally_board_record: dict):
+    #     win = self.win_inspector.check_if_someone_wins(get_horizontally_board_record)
+    #     assert win == "O"
